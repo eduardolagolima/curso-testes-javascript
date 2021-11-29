ae4d8da0056275773c463d6b00d8cd6ff9745f17
Eduardo Lago Lima <eduardolagolima@gmail.com>
2021-11-29 19:17:02 -0300

    adiciona regra para remover produtos duplicados

--- modulo-01/src/lib/Cart/Cart.js
+++ modulo-01/src/lib/Cart/Cart.js
@@ -1,7 +1,16 @@
+import remove from 'lodash/remove';
+import find from 'lodash/find';
+
 class Cart {
   items = [];
 
   add(item) {
+    const itemToFind = { product: item.product };
+
+    if (find(this.items, itemToFind)) {
+      remove(this.items, itemToFind);
+    }
+
     this.items.push(item);
   }
 
