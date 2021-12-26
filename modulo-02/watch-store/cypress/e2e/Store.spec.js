2f04634c9aa7c0b4d55b75769aadf35a069ffe89
Eduardo Lago Lima <eduardolagolima@gmail.com>
2021-12-26 16:57:12 -0300

    finaliza teste e2e da busca de produtos

--- modulo-02/watch-store/cypress/e2e/Store.spec.js
+++ modulo-02/watch-store/cypress/e2e/Store.spec.js
@@ -20,7 +20,7 @@ context('Store', () => {
     cy.get('body').contains('Wrist Watch')
   })
 
-  context('Store > Search for Products', () => {
+  context.only('Store > Search for Products', () => {
     it('should type in the search field', () => {
       cy.visit('http://localhost:3000')
 
@@ -29,20 +29,24 @@ context('Store', () => {
         .should('have.value', 'Some text here')
     })
 
-    it.only('should type in the search field', () => {
-      server.create('product', {
-        title: 'Relógio bonito',
-      })
-
+    it('should return 1 product when "Relógio bonito" is used as search term', () => {
+      server.create('product', { title: 'Relógio bonito' })
       server.createList('product', 10)
 
       cy.visit('http://localhost:3000')
-
       cy.get('input[type="search"]').type('Relógio bonito')
-
       cy.get('[data-testid="search-form"]').submit()
-
       cy.get('[data-testid="product-card"]').should('have.length', 1)
+    })
+
+    it('should not return any product', () => {
+      server.createList('product', 10)
+
+      cy.visit('http://localhost:3000')
+      cy.get('input[type="search"]').type('Relógio bonito')
+      cy.get('[data-testid="search-form"]').submit()
+      cy.get('[data-testid="product-card"]').should('have.length', 0)
+      cy.get('body').contains('0 Products')
     })
   })
 })
