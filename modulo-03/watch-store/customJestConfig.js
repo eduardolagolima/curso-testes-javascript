d93c103e6f4dd19cdd5b1d7507744eaa6ef9b6bb
Eduardo Lago Lima <eduardolagolima@gmail.com>
2022-01-15 19:16:08 -0300

    adiciona teste unitário para o hook personalizado useFetchProducts

--- modulo-03/watch-store/customJestConfig.js
+++ modulo-03/watch-store/customJestConfig.js
@@ -2,7 +2,11 @@ const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
-  collectCoverageFrom: ['<rootDir>/components/**/*.js', '<rootDir>/pages/**/*.js'],
+  collectCoverageFrom: [
+    '<rootDir>/components/**/*.js',
+    '<rootDir>/pages/**/*.js',
+    '<rootDir>/hooks/**/*.js',
+  ],
 };
 
 module.exports = customJestConfig;
