b6c6957b1d25ca741bae018ffbe2b68c8c8de25a
Eduardo Lago Lima <eduardolagolima@gmail.com>
2022-01-25 15:56:53 -0300

    adiciona unit teste para a funcionalidade de remover do componente cart-item

--- modulo-03/watch-store/components/cart-item.unit.spec.js
+++ modulo-03/watch-store/components/cart-item.unit.spec.js
@@ -1,5 +1,11 @@
 import { screen, render, fireEvent } from '@testing-library/react';
 import CartItem from './cart-item';
+import userEvent from '@testing-library/user-event';
+import { renderHook } from '@testing-library/react-hooks';
+import { useCartStore } from '../store/cart';
+import { setAutoFreeze } from 'immer';
+
+setAutoFreeze(false);
 
 const product = {
   title: 'Relógio bonito',
@@ -72,5 +78,20 @@ describe('CartItem', () => {
     fireEvent.click(buttonDecrease);
 
     expect(quantity.textContent).toBe('0');
+  });
+
+  it('should call remove() when remove button is clicked', () => {
+    const result = renderHook(() => useCartStore()).result;
+
+    const spy = jest.spyOn(result.current.actions, 'remove');
+
+    renderCartItem();
+
+    const button = screen.getByRole('button', { name: /remove/i });
+
+    userEvent.click(button);
+
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy).toHaveBeenCalledWith(product);
   });
 });
