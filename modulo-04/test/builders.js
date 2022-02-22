4a05e909391785fc6a0673f0f4016b2ca7ad1ab8
Eduardo Lago Lima <eduardolagolima@gmail.com>
2022-02-22 10:47:12 -0300

    adiciona teste unitário para o service orders

--- modulo-04/test/builders.js
+++ modulo-04/test/builders.js
@@ -2,6 +2,7 @@ import * as validator from 'express-validator';
 import { appError } from '@/utils';
 import userStub from 'test/stubs/user.json';
 import orderStub from 'test/stubs/order.json';
+import ordersStub from 'test/stubs/orders.json';
 
 export function buildUser() {
   return userStub;
@@ -9,6 +10,10 @@ export function buildUser() {
 
 export function buildOrder() {
   return orderStub;
+}
+
+export function buildOrders() {
+  return ordersStub;
 }
 
 export function buildReq({ user = buildUser(), ...overrides } = {}) {
